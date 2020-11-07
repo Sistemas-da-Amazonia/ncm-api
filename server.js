b6c3db5d3dde1bc143c6faac183c93a84ecb7166
pauloarn <44753954+pauloarn@users.noreply.github.com>
2020-11-07 10:48:35 -0300

    PORT CONFIG

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const routes = require('./src/routes');
 const app = express();
 app.use(express.json());
 app.use(routes);
-app.listen(process.env.PORT || 3333);
+app.listen(process.env.PORT_SERVER || 3333);
